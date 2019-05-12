e1d5128dd554180910551f9b5e56f1c8ced99bf6
ccg5230 <ccg5230@163.com>
2019-05-12 23:00:05 +0800

    springcloud consul项目

---
+++ spring-cloud-consul/consul-provider/src/main/java/consul/provider/ConsulProviderApplication.java
@@ -0,0 +1,47 @@
+package consul.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * consul-server 的启动主类
+ * 为了简化代码，我们将 Controller 代码放在主类中，实际工作中不建议这么做
+ */
+@RestController
+@SpringBootApplication
+public class ConsulProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulProviderApplication.class, args);
+    }
+
+    /**
+     * 注意：新版Spring Cloud Consul 默认注册健康检查接口为：/actuator/health
+     *
+     * @return SUCCESS
+
+    @GetMapping("/actuator/health")
+    public String health() {
+        return "SUCCESS";
+    }
+     */
+
+    /**
+     * 这里我们不使用默认的健康检测, 而是使用自己定义的接口
+     * @return SUCCESS
+     */
+    @GetMapping("/health")
+    public String health(){
+        return "SUCCESS";
+    }
+
+    /**
+     * 提供 sayHello 服务:根据对方传来的名字 XX，返回:hello XX
+     * @return String
+     */
+    @GetMapping("/sayHello")
+    public String sayHello(String name){
+        return "hello," + name;
+    }
+}
